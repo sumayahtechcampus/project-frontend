194431baaf9820ef06ce398d62631f380f19657c
YOUR GITHUB nawal <YOUR GITHUB nawaldee21@gmail.com>
2019-12-18 12:55:54 +0300

    sign-up form

--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import './signup.scss';
 
 import { signUp, signIn } from '../api'
 import messages from '../messages'
 
 class SignUp extends Component {
-  constructor () {
+  constructor() {
     super()
 
     this.state = {
@@ -34,92 +35,80 @@ class SignUp extends Component {
       .then(() => history.push('/'))
       .catch(error => {
         console.error(error)
-        this.setState({ email: '', password: '', passwordConfirmation: '', age:'', phone:'', relation:''  })
+        this.setState({ email: '', password: '', passwordConfirmation: '', age: '', phone: '', relation: '' })
         alert(messages.signUpFailure, 'danger')
       })
   }
 
-  render () {
-    const { email, password, passwordConfirmation, age, phone, relation  } = this.state
+  render() {
+    const { email, password, passwordConfirmation, age, phone, relation } = this.state
 
     return (
-      <div> 
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 col-xl-9 mx-auto">
+              <div className="card card-signin flex-row my-5">
+                <div className="card-img-left d-none d-md-flex">
+                  {/* background img for card set in css  */}
+                </div>
 
-        <div class="container">
-    <div class="row">
-      <div class="col-lg-10 col-xl-9 mx-auto">
-        <div class="card card-signin flex-row my-5">
-          <div class="card-img-left d-none d-md-flex">
-           {/* background img for card set in css  */}
+                <div className="card-body">
+                  <h5 className="card-title text-center">Register</h5>
+                  <form className="form-signin">
+                    <div className="form-label-group">
+                      <form className='auth-form' onSubmit={this.onSignUp}>
+                        <h3>Sign Up</h3>
+                      </form>
+
+                    </div>
+
+                    <div className="form-label-group">
+                      {/* <label htmlFor="Email">Email</label> */}
+                      <input required name="email" id="inputEmail" className="form-control" value={email} type="email" placeholder="Email" 
+                        onChange={this.handleChange} />
+                    </div>
+
+                    {/* <hr> */}
+                      <div className="form-label-group">
+                        <label htmlFor="password"></label>
+                        <input required name="password" id="inputPassword" className="form-control" value={password} type="password" placeholder="Password"
+                          onChange={this.handleChange} />
+                      </div>
+                    {/* </hr> */}
+                    
+
+                    <div className="form-label-group">
+                      <label htmlFor="passwordConfirmation"></label>
+                      <input required name="passwordConfirmation" id="inputConfirmPassword" className="form-control" value={passwordConfirmation} type="password"  placeholder="Confirm Password"
+                        onChange={this.handleChange} />
+                    </div>
+
+                    <div className="form-label-group">
+                      <label htmlFor="Age"> </label>
+                      <input required name="age" value={age} type="number" id="inputAge" className="from-control" placeholder="Enter your Age" onChange={this.handleChange} />
+                    </div>
+
+                    <div className="form-label-group">
+                      <label htmlFor="phone"> </label>
+                      <input required name="phone" id="inputPhone" className="from-control" value={phone} type="text" placeholder="Enter your Phone " onChange={this.handleChange} />
+                    </div>
+
+                    <div className="form-label-group">
+                      <label htmlFor="relation"> </label>
+                      <input required name="relation" value={relation} type="text" id="inputRelation" className="from-control" placeholder="Enter your Relation"
+                        onChange={this.handleChange} />
+
+                      <button className="btn btn-lg btn-block text-uppercase btn-pos" type="submit">Sign Up</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
-
-           <div class="card-body">
-            <h5 class="card-title text-center">Register</h5>
-            <form class="form-signin"/>
-              <div class="form-label-group">
-      <form className='auth-form' onSubmit={this.onSignUp}>
-        <h3>Sign Up</h3>
-
-        <label >Email</label>
-        <input required name="email" value={email} type="email" placeholder="Email"
-          onChange={this.handleChange}/>
-           </div>
-
-           
-        <label htmlFor="password">Password</label>
-        <input
-          required
-          name="password"
-          value={password}
-          type="password"
-          placeholder="Password"
-          onChange={this.handleChange}
-          />
-        <label htmlFor="passwordConfirmation">Confirm Password</label>
-        <input
-          required
-          name="passwordConfirmation"
-          value={passwordConfirmation}
-          type="password"
-          placeholder="Confirm Password"
-          onChange={this.handleChange}
-          />
-        <label>Age </label>
-        <input
-          required
-          name="age"
-          value={age}
-          type="number"
-          placeholder="Enter your Age "
-          onChange={this.handleChange}
-          />
-        <label>Phone </label>
-        <input
-          required
-          name="phone"
-          value={phone}
-          type="number"
-          placeholder="Enter your Phone "
-          onChange={this.handleChange}
-          />
-
-<label>Relation </label>
-        <input
-          required
-          name="relation"
-          value={relation}
-          type="text"
-          placeholder="Enter your Relation"
-          onChange={this.handleChange}
-          />
-        
-        <button type="submit">Sign Up</button>
-      </form>
+        </div>
       </div>
-      </div>
-      </div>
-      </div>
-          </div>
+
     )
   }
 }
