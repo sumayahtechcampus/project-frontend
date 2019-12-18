831253dca879f3811d5e1d7eb319d90770c008dd
sumayahtechcampus <sumayah.ali.saleh@gmail.com>
2019-12-18 23:57:33 +0300

    styling for children page

--- src/centeres/api.js
+++ src/centeres/api.js
@@ -4,4 +4,15 @@ import axios from 'axios';
 // Get All Centeres
 export const getAllCenteres = function() {
   return axios.get(`${apiUrl}/centers`);
+
+  
+}
+export const getAllChildrens = function(user) {
+  return axios({
+    url: `${apiUrl}/childrens`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${user.token}` // FOR EXPRESS
+    },
+  });
 }
--- src/children/components/children.js
+++ src/children/components/children.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import profile from './profile.png'
 
 class Children extends React.Component {
   
@@ -6,11 +7,78 @@ class Children extends React.Component {
   render() {
     return (
       <div>
-        <h2>{this.props.name}</h2>
-        <sub>{this.props.age}</sub>
-        <p>{this.props.interest}</p>
-        <p> {this.props.school}</p>
-       <p> {this.props.allergy} </p>
+        <div className="container">
+ <div className="row">
+ <div className="col-12">
+ <div className="card">
+    
+ <div class="card-body">
+                         <div className="card-title mb-4">
+                             <div className="d-flex justify-content-start">
+                                 <div >
+                                     <img src={profile} width="150px" height="150"/>
+                                    
+                                 </div>
+                                 <div className="userData ml-5">
+                                   <br /> 
+                                   <br /> 
+                                     <h2 className="d-block">{this.props.name}</h2>
+                                
+                                 </div>
+                                <div className="ml-auto">
+                                     <input type="button" className="btn btn-primary d-none" id="btnDiscard" value="Discard Changes" />
+                                 </div>
+                             </div>
+                        </div>
+
+
+                                       
+
+
+                                         <div className="row">
+                                             <div className="col-sm-3 col-md-2 col-5">
+                                                 <label font-weight="bold">Age : </label>
+                                             </div>
+                                             <div className="col-md-8 col-6">
+                                             {this.props.age}
+                                             </div>
+                                        </div>
+
+
+                                             <div className="row">
+                                             <div className="col-sm-3 col-md-2 col-5">
+                                                 <label font-weight="bold">Relation : </label>
+                                             </div>
+                                             <div className="col-md-8 col-6">
+                                            {this.props.interest}
+                                             </div>
+                                        </div>
+
+
+                                         <div className="row">
+                                             <div className="col-sm-3 col-md-2 col-5">
+                                                <label font-weight="bold">Phone : </label>
+                                            </div>
+                                            <div className="col-md-8 col-6">
+                                            {this.props.school}
+                                             </div>
+                                       </div> 
+                                       <div className="row">
+                                           <div className="col-sm-3 col-md-2 col-5">
+                                                <label font-weight="bold">Phone : </label>
+                                           </div>
+                                            <div className="col-md-8 col-6">
+                                            {this.props.allergy}                                          
+                                            </div>
+                                     </div> 
+
+
+      </div>
+      </div>
+      </div>
+      </div>
+
+   </div> 
       
       </div>
     );
@@ -18,3 +86,18 @@ class Children extends React.Component {
 }
 
 export default Children;
+
+
+
+
+
+
+//<div>
+      //   <h2>{this.props.name}</h2>
+      //   <sub>{this.props.age}</sub>
+      //   <p>{this.props.interest}</p>
+      //   <p> {this.props.school}</p>
+      //  <p> {this.props.allergy} </p>
+      
+      // </div>
+  
