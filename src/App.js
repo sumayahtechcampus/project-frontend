c9a777ac71901c0871ce3d72bb3133298df8bc13
saraOmar18 <sezla18@gmail.com>
2019-12-18 13:17:51 +0300

    edit app component

--- src/App.js
+++ src/App.js
@@ -75,53 +75,7 @@ class App extends Component {
             <MyProfile alert={this.alert} user={user} />
           )} />
           </main>
-<<<<<<< HEAD
-            {/* <Carousel> 
-            <Carousel.Item>
-    <img
-      className="d-block "
-      src={slider1}
-      alt="First slide"
-      width="1285px"
-      height="470px"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block "
-      src={slider2}
-      alt="Third slide"
-      width="1285px"
-      height="470px"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block "
-      src={slider3}
-      width="1285px"
-      height="470px"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-            </Carousel> */
-            }
-{/* <Footer /> */}
-=======
           <Footer />
->>>>>>> 2355d8dd275bef98ce536642b89db41902b6ae10
       </React.Fragment>
     )
   }
